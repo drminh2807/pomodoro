85b34e801c1965f0a8c89bbd8a5afc1921c53003
drminh2807 <drminh2807@gmail.com>
2024-09-06 14:01:08 +0700

    try login page

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,30 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", {
-      jsxImportSource: "nativewind"
-    }], "nativewind/babel"],
+    presets: [
+      [
+        "babel-preset-expo",
+        {
+          jsxImportSource: "nativewind",
+        },
+      ],
+      "nativewind/babel",
+    ],
 
-    plugins: [["module-resolver", {
-      root: ["./"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
 
-      alias: {
-        "@": "./"
-      }
-    }]]
+          alias: {
+            "@": "./",
+            "@unitools/image": "@unitools/image-expo",
+            "@unitools/router": "@unitools/router-expo",
+            "@unitools/link": "@unitools/link-expo",
+          },
+        },
+      ],
+    ],
   };
 };
